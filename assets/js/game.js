4a025db53151e0ee447997e0c057f4c9f0371e57
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-02-15 21:33:40 -0700

    Underscore and create deck

---
+++ assets/js/game.js
@@ -0,0 +1,21 @@
+let deck = [];
+const suits = ['C','D','H','S'];
+const courtCards = ['A','J','Q','K'];
+
+const createDeck = () => {
+    for( let i = 2; i <= 10; i++) {
+        for( let suit of suits) {
+            deck.push( i + suit);
+        }
+    }
+
+    for( let suit of suits ) {
+        for( let courtCard of courtCards) {
+            deck.push( courtCard + suit);
+        }
+    }
+    deck = _.shuffle( deck );
+    return deck;
+}
+
+createDeck();
