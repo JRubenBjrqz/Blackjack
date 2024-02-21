838fcc9be3be7dc72433b15384fc5ef22011fe32
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-02-20 21:49:56 -0700

    Value card

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,8 @@ const courtCards = ['A','J','Q','K'];
 
 const createDeck = () => {
     for( let i = 2; i <= 10; i++) {
-        for( let suit of suits) {
-            deck.push( i + suit);
+        for( let suit of suits ) {
+            deck.push( i + suit );
         }
     }
 
@@ -28,4 +28,18 @@ const hitCard = () => {
     return card;
 }
 
-hitCard();
+const cardValue = ( card ) => {
+    const value = card.substring( 0, card.length - 1 );
+    return ( isNaN( value ) ) ?
+            ( value === 'A' ) ? 11 : 10
+            : value * 1;
+    // points = 0;
+
+    // if( isNaN( value ) ){
+    //     points = ( value === 'A' ) ? 11 : 10;
+    // } else {
+    //     points = value * 1;
+    // }
+}
+
+cardValue('2D');
