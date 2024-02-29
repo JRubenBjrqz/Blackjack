66c136939029fbc374e2dee41157ba5a10d4aec5
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-02-28 18:53:52 -0700

    Module pattern

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,161 +1,165 @@
-let deck = [];
-const suits = ['C', 'D', 'H', 'S'];
-const courtCards = ['A', 'J', 'Q', 'K'];
 
-let playerPoints = 0,
-    pcPoints = 0;
+(() => {
+    'use strict'
 
-const hitButton = document.querySelector('#hitButton');
-const standButton = document.querySelector('#standButton');
-const newGameButton = document.querySelector('#newGameButton');
+    let deck = [];
+    const suits = ['C', 'D', 'H', 'S'];
+    const courtCards = ['A', 'J', 'Q', 'K'];
 
-const displayPlayerCards = document.querySelector('#playerCards');
-const displayPCCards = document.querySelector('#pcCards');
+    let playerPoints = 0,
+        pcPoints = 0;
 
-const displayPoints = document.querySelectorAll('strong');
+    const hitButton = document.querySelector('#hitButton');
+    const standButton = document.querySelector('#standButton');
+    const newGameButton = document.querySelector('#newGameButton');
 
-const createDeck = () => {
-    for (let i = 2; i <= 10; i++) {
+    const displayPlayerCards = document.querySelector('#playerCards');
+    const displayPCCards = document.querySelector('#pcCards');
+
+    const displayPoints = document.querySelectorAll('strong');
+
+    const createDeck = () => {
+        for (let i = 2; i <= 10; i++) {
+            for (let suit of suits) {
+                deck.push(i + suit);
+            }
+        }
+
         for (let suit of suits) {
-            deck.push(i + suit);
+            for (let courtCard of courtCards) {
+                deck.push(courtCard + suit);
+            }
         }
+        deck = _.shuffle(deck);
+        console.log(deck);
+        return deck;
     }
 
-    for (let suit of suits) {
-        for (let courtCard of courtCards) {
-            deck.push(courtCard + suit);
+    createDeck();
+
+    const hitCard = () => {
+        if (deck.length === 0) {
+            throw 'Empty deck';
         }
+        const card = deck.pop();
+        return card;
     }
-    deck = _.shuffle(deck);
-    console.log(deck);
-    return deck;
-}
 
-createDeck();
+    const cardValue = (card) => {
+        const value = card.substring(0, card.length - 1);
 
-const hitCard = () => {
-    if (deck.length === 0) {
-        throw 'Empty deck';
+        return (isNaN(value)) ?
+            (value === 'A') ? 11 : 10
+            : value * 1;
+        // points = 0;
+
+        // if( isNaN( value ) ){
+        //     points = ( value === 'A' ) ? 11 : 10;
+        // } else {
+        //     points = value * 1;
+        // }
     }
-    const card = deck.pop();
-    return card;
-}
 
-const cardValue = (card) => {
-    const value = card.substring(0, card.length - 1);
+    // PC Turn
 
-    return (isNaN(value)) ?
-        (value === 'A') ? 11 : 10
-        : value * 1;
-    // points = 0;
+    const pcTurn = ( minimumPoints ) => {
+        
+        do {
+            const card = hitCard();
 
-    // if( isNaN( value ) ){
-    //     points = ( value === 'A' ) ? 11 : 10;
-    // } else {
-    //     points = value * 1;
-    // }
-}
+            pcPoints = pcPoints + cardValue(card);
+            displayPoints[0].innerText = pcPoints;
 
-// PC Turn
+            const imgCard = document.createElement('img');
 
-const pcTurn = ( minimumPoints ) => {
-    
-    do {
+            imgCard.src = `assets/img/cards/${card}.png`;
+            imgCard.classList.add('card');
+            displayPCCards.append(imgCard);
+
+            if( minimumPoints > 21 ) {
+                break;
+            }
+
+        } while( ( pcPoints < minimumPoints && minimumPoints <= 21 ) );
+
+        setTimeout(() => {
+
+            if( pcPoints === minimumPoints ) {
+                alert('Tie');
+            } else if ( minimumPoints > 21 ) {
+                alert('PC Win');
+            } else if( pcPoints > 21 ){
+                alert('Player Win');
+            } else {
+                alert('PC Win');
+            }
+
+        }, 250 );
+
+    }
+
+    // Events
+
+    hitButton.addEventListener('click', () => {
         const card = hitCard();
 
-        pcPoints = pcPoints + cardValue(card);
-        displayPoints[0].innerText = pcPoints;
+        playerPoints = playerPoints + cardValue(card);
+        displayPoints[1].innerText = playerPoints;
 
         const imgCard = document.createElement('img');
 
         imgCard.src = `assets/img/cards/${card}.png`;
         imgCard.classList.add('card');
-        displayPCCards.append(imgCard);
+        displayPlayerCards.append(imgCard);
 
-        if( minimumPoints > 21 ) {
-            break;
+        if (playerPoints > 21) {
+            console.log('Loser');
+            hitButton.disabled = true;
+            hitButton.classList.add('disabled:opacity-75');
+            standButton.disabled = true;
+            standButton.classList.add('disabled:opacity-75');
+            pcTurn( playerPoints );
+
+        } else if (playerPoints === 21) {
+            console.log('Winner');
+            hitButton.disabled = true;
+            hitButton.classList.add('disabled:opacity-75');
+            standButton.disabled = true;
+            standButton.classList.add('disabled:opacity-75');
+            pcTurn( playerPoints );
+
         }
+    });
 
-    } while( ( pcPoints < minimumPoints && minimumPoints <= 21 ) );
+    standButton.addEventListener('click', () => {
 
-    setTimeout(() => {
-
-        if( pcPoints === minimumPoints ) {
-            alert('Tie');
-        } else if ( minimumPoints > 21 ) {
-            alert('PC Win');
-        } else if( pcPoints > 21 ){
-            alert('Player Win');
-        } else {
-            alert('PC Win');
-        }
-
-    }, 250 );
-
-}
-
-// Events
-
-hitButton.addEventListener('click', () => {
-    const card = hitCard();
-
-    playerPoints = playerPoints + cardValue(card);
-    displayPoints[1].innerText = playerPoints;
-
-    const imgCard = document.createElement('img');
-
-    imgCard.src = `assets/img/cards/${card}.png`;
-    imgCard.classList.add('card');
-    displayPlayerCards.append(imgCard);
-
-    if (playerPoints > 21) {
-        console.log('Loser');
         hitButton.disabled = true;
         hitButton.classList.add('disabled:opacity-75');
         standButton.disabled = true;
         standButton.classList.add('disabled:opacity-75');
+
         pcTurn( playerPoints );
 
-    } else if (playerPoints === 21) {
-        console.log('Winner');
-        hitButton.disabled = true;
-        hitButton.classList.add('disabled:opacity-75');
-        standButton.disabled = true;
-        standButton.classList.add('disabled:opacity-75');
-        pcTurn( playerPoints );
+    });
 
-    }
-});
+    newGameButton.addEventListener('click', () => {
 
-standButton.addEventListener('click', () => {
+        deck = []
+        createDeck();
+        
+        playerPoints = 0;
+        pcPoints = 0;
 
-    hitButton.disabled = true;
-    hitButton.classList.add('disabled:opacity-75');
-    standButton.disabled = true;
-    standButton.classList.add('disabled:opacity-75');
+        displayPoints[0].innerText = 0;
+        displayPoints[1].innerText = 0;
 
-    pcTurn( playerPoints );
+        displayPCCards.innerHTML = '';
+        displayPlayerCards.innerHTML = '';
+        hitButton.disabled = false;
+        hitButton.classList.remove('disabled:opacity-75');
+        standButton.disabled = false;
+        standButton.classList.remove('disabled:opacity-75');
 
-});
+    });
 
-newGameButton.addEventListener('click', () => {
-
-    deck = []
-    createDeck();
-    
-    playerPoints = 0;
-    pcPoints = 0;
-
-    displayPoints[0].innerText = 0;
-    displayPoints[1].innerText = 0;
-
-    displayPCCards.innerHTML = '';
-    displayPlayerCards.innerHTML = '';
-    hitButton.disabled = false;
-    hitButton.classList.remove('disabled:opacity-75');
-    standButton.disabled = false;
-    standButton.classList.remove('disabled:opacity-75');
-
-});
-
-
+})();
